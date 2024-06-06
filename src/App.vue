7cbb4b4cc0007963edeeb18e2b37dacc75177710
HuangWen <2493679472@qq.com>
2024-06-06 21:59:48 +0800

    home

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
+  <router-link to="/"></router-link>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+
+    <!--    <router-link to="/about">About</router-link>-->
   </nav>
   <router-view/>
 </template>
@@ -11,20 +12,22 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  //text-align: center;
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+//nav {
+//  padding: 30px;
+//
+//  a {
+//    font-weight: bold;
+//    color: #2c3e50;
+//
+//    &.router-link-exact-active {
+//      color: #42b983;
+//    }
+//  }
+//}
 </style>
+<script setup lang="ts">
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,247 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="header">
+      <div>智能温室管理系统</div>
+      <div class="my-home"><el-button class="my-btn" plain>个人主页</el-button></div>
+    </div>
+    <div class="main">
+      <div class="left">
+        <div class="left-up">
+          <el-button class="btn" plain>设置</el-button>
+          <el-button class="btn" plain>切换</el-button>
+          <el-button class="btn" plain>刷新</el-button>
+        </div>
+        <div class="left-down">
+          <el-button class="warning-btn" plain>报警记录</el-button>
+        </div>
+      </div>
+      <div class="middle">
+        <div class="middle-up">
+          <div class="border-box" style="width: 730px;height: 370px;"><div id="chart"></div></div>
+        </div>
+        <div class="middle-down">
+          <div style="width: 730px;height: 220px;">
+            <!--            <el-scrollbar class="scroll"  max-height="220px">-->
+            <!--              -->
+            <!--            </el-scrollbar>-->
+            <div class="border-box">
+              <el-table :data="tableData" height="220">
+                <el-table-column prop="date" label="Date"/>
+                <el-table-column prop="name" label="Name" />
+                <el-table-column prop="address" label="Address" />
+              </el-table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="right">
+        <el-table :data="tableData" fit>
+          <el-table-column prop="date" label="Date"/>
+          <el-table-column prop="name" label="Name" />
+          <el-table-column prop="address" label="Address" />
+        </el-table>
+      </div>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script setup lang="ts">
+// import { Vue } from 'vue-class-component';
+import * as echarts from "echarts";
+import {onMounted, ref} from "vue";
 
-@Options({
-  components: {
-    HelloWorld,
+//声明周期函数，自动执行初始化
+onMounted(() => {
+  init();
+});
+//初始化函数
+function init() {
+  let chartDom = document.getElementById('chart');
+  // var myChart = echarts.init(chartDom, 'dark');
+  let myChart = echarts.init(chartDom, 'dark');
+  let option;
+
+  option = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: [150, 230, 224, 218, 135, 147, 260],
+        type: 'line'
+      }
+    ]
+  };
+  option && myChart.setOption(option);
+}
+
+
+const value = ref('')
+const options = [
+  {
+    value: 'Option1',
+    label: 'Option1',
   },
-})
-export default class HomeView extends Vue {}
+  {
+    value: 'Option2',
+    label: 'Option2',
+  }
+]
+
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+]
+
+const activeNames = ref(['1'])
+const handleChange = (val: string[]) => {
+  console.log(val)
+}
 </script>
+
+<style scoped>
+.home {
+  color: #fff;
+  font-size: 30px;
+  position: fixed; /* 使背景固定 */
+  top: 0;
+  left: 0;
+  width: 100vw; /* 视口宽度 */
+  overflow: hidden;
+  z-index: -1; /* 确保背景在其他内容下方 */
+  background-image: url('../assets/bg.png');
+  background-size: 100% 100%;
+  box-shadow: 0 0 3px blue;
+}
+.header {
+  text-align: center;
+  margin: 20px 0 10px 0;
+
+}
+.my-home {
+  position: absolute;
+  top: 20px;
+  right: 100px;
+}
+.my-btn {
+  color: #ffffff;
+  background-color: #2c31;
+
+}
+.main {
+  display: flex;
+//align-items: center;
+  justify-content: space-around;
+//height: 100%;
+}
+.border-box {
+  border: 1px solid #5d6c99;
+}
+.left {
+//flex: 1;
+  width: 15%;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+//justify-content: space-between;
+//background-color: aquamarine;
+  margin-right: 10px;
+}
+.middle {
+//flex: 1;
+  width: 55%;
+  display: flex;
+  flex-direction: column;
+  margin-right: 10px;
+}
+.right {
+//flex: 1;
+  width: 30%;
+  margin-right: 10px;
+}
+
+.left-up {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+//height: 70%;
+//background-color: aquamarine;
+}
+
+.btn {
+  width: 100px;
+  height: 50px;
+  margin: 20px;
+  color: #ffffff;
+  background-color: #2c31;
+}
+.left-down {
+//height: 30%;
+//background-color: #42b983;
+}
+.warning-btn {
+  width: 100px;
+  height: 50px;
+  margin-top: 200px;
+  color: #ffffff;
+  background-color: #2c31;
+}
+.middle-up {
+  display: flex;
+  justify-content: center;
+}
+
+.middle-down {
+  margin: 30px 0 12px 0;
+//padding: 10px 0 10px 0;
+  display: flex;
+  justify-content: center;
+//height: 400px;
+}
+.el-table{
+  background-color: #2c31 !important;
+//color: #ffffff;
+}
+.scroll{
+  width: 730px;
+  height: 220px;
+  padding: 5px;
+
+}
+.el-collapse[data-v-9ea40744] {
+  background-color: #2c31 !important;
+  color: #ffffff !important;
+}
+/* 样式这里要设置长宽，不然显示不出来 */
+#chart {
+  width: 730px;
+  height: 370px;
+}
+</style>
